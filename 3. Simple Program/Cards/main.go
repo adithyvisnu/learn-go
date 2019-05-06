a03786c26e1e8447c93b60078a64d56074b5583b
adithyvisnu <adithyavisnu@gmail.com>
2019-05-06 13:05:10 +0700

    function declarations

--- 3. Simple Program/Cards/main.go
+++ 3. Simple Program/Cards/main.go
@@ -25,4 +25,15 @@ func main() {
 	var i int
 	i = 1002
 	fmt.Println(i)
+
+	newc := newCard()
+	fmt.Println(newc)
+}
+
+// declare a new function that have
+// newCard is the name of function
+// string is the return type from
+//        the function when its invoked
+func newCard() string {
+	return "Five of Clovers"
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # learn-go
 This is a summary for learning go by myself
+
+### Source
+Udemy - Go: The Complete Developer's Guide (Golang) by Stephen Grider
