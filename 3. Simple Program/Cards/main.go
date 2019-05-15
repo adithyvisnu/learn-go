546c32f398d1eaeb7aa43e5ee7406b87c0142a91
adithyvisnu <adithyavisnu@gmail.com>
2019-05-15 22:19:25 +0700

    create a newDeck function

--- 3. Simple Program/Cards/main.go
+++ 3. Simple Program/Cards/main.go
@@ -3,7 +3,17 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Spades", "Five of Clovers"}
-	cards = append(cards, "Ten of Hearts")
+	// this one the first approach for learning
+	// the function receiver method
+	// it commented due to creating a new code
+	// for initializing a deck itself
+	// ==============================================
+	// cards := deck{"Ace of Spades", "Five of Clubs"}
+	// cards = append(cards, "Ten of Hearts")
+	// cards.print()
+
+	// this is a new code for declaring a deck
+	// declaring deck with the new function
+	cards := newDeck()
 	cards.print()
 }
