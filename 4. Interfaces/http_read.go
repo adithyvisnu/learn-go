c14a9e028a97c1f622948221e8d02078b41b2fc0
adithyvisnu <adithyavisnu@gmail.com>
2019-06-08 17:39:44 +0700

    creating own function that implement Writer interface

--- 4. Interfaces/http_read.go
+++ 4. Interfaces/http_read.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type customWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,7 +16,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+	cw := customWriter{}
+	io.Copy(cw, resp.Body)
+}
+
+func (cw customWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs), len(bs))
+	return len(bs), nil
 }
